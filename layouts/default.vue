6bc7daba29fecb65f1a0de4e262ec4bafaf47636
Jose Bravo <mjabravo221@gmail.com>
2020-04-14 22:25:01 -0700

    boilerplate

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <top-header/>
+    <main id="main" role="main">
+      <nuxt/>
+    </main>
+    <bottom-footer/>
+  </div>
+</template>
+
+<script>
+import TopHeader from '~/components/TopHeader.vue'
+import BottomFooter from '~/components/BottomFooter.vue'
+
+export default {
+  components: {
+    TopHeader,
+    BottomFooter
+  }
+}
+</script>
