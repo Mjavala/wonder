b7f362b44936a60aafaa691b8dd187acf423324a
Jose Bravo <mjabravo221@gmail.com>
2020-05-04 22:39:41 -0700

    page 2, border box not fully styled

--- components/wonder.vue
+++ components/wonder.vue
@@ -1,55 +1,140 @@
 <template>
-  <div>
-    <div v-show="this.animLoaded" id="main" role="main" />
+  <div id="wrapper">
+    <nav id="nav">
+      <svg xmlns="http://www.w3.org/2000/svg" class="hamburger" viewBox="0 0 36.167 24.922" preserveAspectRatio="xMidYMid meet" @click="showSideMenu = true"><g transform="translate(-6377.605 -701)"><rect width="36.167" height="5.167" transform="translate(6377.605 701)" fill="#fff" /><rect width="36.167" height="5.167" transform="translate(6377.605 710.877)" fill="#fff" /><rect width="36.167" height="5.167" transform="translate(6377.605 720.755)" fill="#fff" /></g></svg>
+      <div id="nav-wrapper">
+        <div class="nav-item">
+          Film
+        </div>
+        <div class="nav-item">
+          Photo
+        </div>
+        <div class="nav-item">
+          About
+        </div>
+        <div class="nav-item">
+          Contact
+        </div>
+      </div>
+    </nav>
+    <div v-if="showSideMenu" id="side-nav">
+      <div class="side-nav-item-wrap">
+        <div class="side-nav-item">
+          Film
+        </div>
+      </div>
+      <div class="side-nav-item-wrap">
+        <div class="side-nav-item">
+          Photo
+        </div>
+      </div>
+      <div class="side-nav-item-wrap">
+        <div class="side-nav-item">
+          About
+        </div>
+      </div>
+      <div class="side-nav-item-wrap">
+        <div class="side-nav-item">
+          Contact
+        </div>
+        <div id="exit-side-menu" @click="showSideMenu = false">
+          X
+        </div>
+      </div>
+    </div>
+    <video autoplay>
+      <source src="@/assets/320.mp4" type="video/mp4">
+    </video>
   </div>
 </template>
 
 <script>
-import lottie from 'lottie-web'
-import * as animationData from '~/assets/final_html.json'
 
 export default {
   data () {
     return {
-      animLoaded: false
+      loaded: false,
+      showSideMenu: false
     }
   },
   mounted () {
     window.addEventListener('load', () => {
-      this.loadAnimation()
+      this.loaded = true
     })
   },
   methods: {
-    loadAnimation () {
-      this.animLoaded = true
-      const anim = lottie.loadAnimation({
-        container: document.getElementById('main'),
-        renderer: 'svg',
-        loop: false,
-        autoplay: false,
-        animationData: animationData.default,
-        rendererSettings: {
-          progressiveLoad: true,
-          hideOnTransparent: true
-        }
-      }
-      )
-      anim.setSubframe = false
-      anim.addEventListener('DOMloaded', this.triggerPlay(anim))
-    },
-    triggerPlay (anim) {
-      console.log('ready anim triggered')
-      console.log(this.animLoaded)
-      anim.play()
-    }
   }
 }
 </script>
 
-<style scoped>
-  #main{
-    padding: 0 1.5625em;
+<style>
+  #wrapper{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1.5625em;
     height: 100vh !important;
     background-color: #FF0000;
+    position: relative;
+  }
+  .hamburger {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 6%;
+    padding: 1.5625em;
+    cursor: pointer;
+  }
+  #nav-wrapper{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 60%;
+    color: white;
+    padding: 1.5625em;
+    font-size: 1em;
+  }
+  .nav-item{
+    cursor: pointer;
+  }
+  #side-nav{
+    height: 100vh;
+    position: absolute;
+    left: 0;
+    top: 0;
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+  }
+  .side-nav-item-wrap {
+    width: 15em;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+  }
+  .side-nav-item-wrap:nth-child(1),
+  .side-nav-item-wrap:nth-child(2),
+  .side-nav-item-wrap:nth-child(3) {
+    border-bottom: 1px solid #FF0000;
+  }
+  .side-nav-item{
+    text-align: left;
+    font-size: 2em;
+    width: 60%;
+  }
+  #exit-side-menu{
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    padding: 1em;
+    color: #FF0000;
+    cursor: pointer;
   }
 </style>
