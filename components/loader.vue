9b449023595dac52f72109e4310647330fe23fcc
Jose Bravo <mjabravo221@gmail.com>
2020-05-28 02:30:25 -0700

    ie 11 loader

--- components/loader.vue
+++ components/loader.vue
@@ -51,16 +51,22 @@ export default {
   }
   .internal-circle {
     stroke-dasharray: 187;
+    -webkit-animation: internal 3s ease-in-out infinite;
+    -moz-animation: internal 3s ease-in-out infinite;;
     animation: internal 3s ease-in-out infinite;
     opacity: .5;
   }
   .external-circle {
     stroke-dasharray: 312;
+    -webkit-animation: external 5s linear infinite;
+    -moz-animation: external 5s linear infinite;
     animation: external 5s linear infinite;
   }
   .external-circle-active{
     stroke: transparent;
     transform-origin: center;
+    -webkit-animation: leave .35s linear 1 forwards;
+    -moz-animation: leave .35s linear 1 forwards;
     animation: leave .35s linear 1 forwards;
   }
   .external-loader-active{
@@ -68,7 +74,25 @@ export default {
     background-image: linear-gradient(315deg,#020299, 33%, #ff0066 66%);
     border-radius: 50%;
     filter: blur(1.5px);
+    -webkit-animation: animate 1s linear 1 forwards, pulse 1s 1 forwards;
+    -moz-animation: animate 1s linear 1 forwards, pulse 1s 1 forwards;
     animation: animate 1s linear 1 forwards, pulse 1s 1 forwards;
+  }
+  @-webkit-keyframes animate {
+      0% {
+          transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(270deg);
+      }
+  }
+  @-moz-keyframes animate {
+      0% {
+          transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(270deg);
+      }
   }
   @keyframes animate {
       0% {
@@ -77,6 +101,28 @@ export default {
       100% {
         transform: rotate(270deg);
       }
+  }
+  @-webkit-keyframes pulse {
+    0% {
+        opacity: .7;
+    }
+    50% {
+        opacity: 0.5;
+    }
+    100% {
+        opacity: 0;
+    }
+  }
+  @-moz-keyframes pulse {
+    0% {
+        opacity: .7;
+    }
+    50% {
+        opacity: 0.5;
+    }
+    100% {
+        opacity: 0;
+    }
   }
   @keyframes pulse {
     0% {
@@ -89,6 +135,22 @@ export default {
         opacity: 0;
     }
   }
+  @-webkit-keyframes leave {
+    0% {
+        opacity: .1;
+    }
+    100% {
+        opacity: 1;
+    }
+  }
+  @-moz-keyframes leave {
+    0% {
+        opacity: .1;
+    }
+    100% {
+        opacity: 1;
+    }
+  }
   @keyframes leave {
     0% {
         opacity: .1;
@@ -97,6 +159,30 @@ export default {
         opacity: 1;
     }
   }
+@-webkit-keyframes internal {
+  0% {
+    stroke-dashoffset: 0;
+  }
+  35% {
+    stroke-dashoffset: -70;
+  }
+  100% {
+    stroke-dashoffset: -187;
+    transform: rotate(1440deg);
+  }
+}
+@-moz-keyframes internal {
+  0% {
+    stroke-dashoffset: 0;
+  }
+  35% {
+    stroke-dashoffset: -70;
+  }
+  100% {
+    stroke-dashoffset: -187;
+    transform: rotate(1440deg);
+  }
+}
 @keyframes internal {
   0% {
     stroke-dashoffset: 0;
@@ -107,6 +193,30 @@ export default {
   100% {
     stroke-dashoffset: -187;
     transform: rotate(1440deg);
+  }
+}
+@-webkit-keyframes external {
+  0% {
+    stroke-dashoffset: 312;
+  }
+  60% {
+    stroke-dashoffset: 100;
+  }
+  100% {
+    stroke-dashoffset: -312;
+    transform: rotate(1800deg);
+  }
+}
+@-moz-keyframes external {
+  0% {
+    stroke-dashoffset: 312;
+  }
+  60% {
+    stroke-dashoffset: 100;
+  }
+  100% {
+    stroke-dashoffset: -312;
+    transform: rotate(1800deg);
   }
 }
 @keyframes external {
